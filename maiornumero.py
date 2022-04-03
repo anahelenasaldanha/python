7537bd2e5f755515d0252278963e54f73a34bf09
Ana Helena <97840856+anahelenasaldanha@users.noreply.github.com>
2022-04-02 22:01:52 -0300

    Add files via upload

---
+++ maiornumero.py
@@ -0,0 +1,13 @@
+a = int(input("Digite o primeiro valor: "))
+b = int(input("Digite o segundo valor: "))
+c = int(input("Digite o terceiro valor: "))
+
+if a > b and a > c:
+    print("O maior número é {}".format(a))
+elif b > a and b > c:
+    print("O maior número é {}".format(b))
+else:
+    print("O maior número é {}".format(c))
+
+print("Final do programa")
+
