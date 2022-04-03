7039da6bcb7703194e8eb3520d2165831a33911f
Ana Helena <97840856+anahelenasaldanha@users.noreply.github.com>
2022-04-02 22:29:46 -0300

    Add files via upload

---
+++ mediaaluno.py
@@ -0,0 +1,12 @@
+a = int(input("Primeiro bimestre: "))
+b = int(input("Segundo bimestre: "))
+c = int(input("Terceiro bimestre: "))
+d = int(input("Quarto bimestre: "))
+
+media = (a + b + c + d) / 4
+
+if a <= 10 and b <= 10 and c <= 10 and d <= 10:
+    print("Média: {}".format(media))
+else:
+    print("Foi informada alguma nota errada!")
+
